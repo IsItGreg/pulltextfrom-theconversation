269d67f4f7cb8ab3b1161abc69cd19b2e3e58706
IsItGreg <gsmelkov@gmail.com>
2018-06-07 10:31:29 -0400

    Fixed initial spaces in files produced by readArticle

--- main.py
+++ main.py
@@ -1,12 +1,36 @@
-import pandas
+import pandas as pd
+from readArticle import readArticle
 
 
 def main():
+    #testurl = "http://theconversation.com/statesman-strongman-philosopher-autocrat-chinas-xi-is-a-man-who-contains-multitudes-92962"
+    #readArticle(testurl, 0)
 
+    curindex = 0
+    indexes = []
+    authors = []
+    titles = []
+    urls = []
+    tags = []
 
+    df = pd.read_csv("tc_articles (7).csv")
 
+    array = df.values.tolist()
+    for list in array:
+        indexes.append("{:0>3d}".format(curindex))
+        authors.append(readArticle(list[1], curindex))
+        titles.append(list[0])
+        urls.append(list[1])
+        tags.append(list[2])
+        curindex += 1
+        print "{:.4f}".format(curindex/6100.0) + "% done"
 
-    print ("1")
+    print ("Done: " + str(curindex))
+
+    data = {'Author': authors, 'Title': titles, 'URL': urls, 'Category' : tags}
+    output = pd.DataFrame(data, index=indexes).rename_axis("Text #", axis=1)
+    output.to_csv('conversationMetadata.txt', sep='\t', encoding='utf-8')
+
 
 if __name__ == "__main__":
     main()
---
+++ readArticle.py
@@ -0,0 +1,45 @@
+import urllib2
+from bs4 import BeautifulSoup
+
+def fixFile(file):
+    lines = []
+    with open(file, "r") as text_file:
+        for line in text_file:
+            if line[0] == ' ':
+                line = line[1:]
+            lines.append(line)
+
+    with open(file, "w") as text_file:
+        for line in lines:
+            text_file.write(line)
+
+
+def readArticle(url, number):
+    try:
+        html = urllib2.urlopen(url)
+    except:
+        return "failed"
+    shouldPrint = False
+    soup = BeautifulSoup(html, 'html.parser')
+    cclicense = soup.find("a", {"class" : "license-logo"})
+    author = soup.find("meta", {"name" : "author"})['content']
+    with open("conversation"+"{:0>3d}".format(number)+".txt", "w") as text_file:
+        for line in soup.find_all('p'):
+            if "Write an article and join a growing community of more than" in str(line):
+                #print "Works"
+                shouldPrint = False
+            if shouldPrint == True:
+                if line.string == None:
+                    for string in line.stripped_strings:
+                        if string[0] not in ['.', ',', '!', '?', '"', "'"]:# and string[0] != string[0].upper():
+                            text_file.write(' ')
+                        text_file.write(string.encode('utf-8'))
+                else:
+                    text_file.write(line.string.encode('utf-8'))
+                text_file.write("\n")
+            elif cclicense in line:
+                shouldPrint = True
+    fixFile("conversation"+"{:0>3d}".format(number)+".txt")
+    return author
+
+
